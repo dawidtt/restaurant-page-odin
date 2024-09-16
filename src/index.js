745d6dd372f79db11578fc45c3a1eec3354ebdc1
dawidtt <dawid.gorszka@gmail.com>
2024-09-16 20:13:19 +0200

    Create function to clear content container and add event listeners on nav buttons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import "./template.css";
 import * as home from "./home/home.js";
-home.renderHomePage();
+
+(function () {
+  function clearContent(e) {
+    homeBtn.classList.remove("btn-shadow");
+    menuBtn.classList.remove("btn-shadow");
+    aboutBtn.classList.remove("btn-shadow");
+
+    const content = document.querySelector("#content");
+    content.innerHTML = "";
+    e.srcElement.classList.add("btn-shadow");
+  }
+  const homeBtn = document.querySelector("#home");
+  const menuBtn = document.querySelector("#menu");
+  const aboutBtn = document.querySelector("#about");
+
+  home.renderHomePage();
+  homeBtn.classList.add("btn-shadow");
+  homeBtn.addEventListener("click", clearContent);
+  homeBtn.addEventListener("click", home.renderHomePage);
+  menuBtn.addEventListener("click", clearContent);
+  menuBtn.addEventListener("click", home.renderHomePage);
+  aboutBtn.addEventListener("click", clearContent);
+  aboutBtn.addEventListener("click", home.renderHomePage);
+})();
